976f324c4f78ef3395d53bda455576cbd7996260
HanyueZheng <zhenghanyue813@gmail.com>
2020-04-03 22:38:57 -0500

    ast

--- Train.py
+++ Train.py
@@ -79,7 +79,7 @@ class SimpleLossCompute:
             # print(self.criterion(x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1).float()))
             # print("loss:")
             # print(self.criterion(x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1).float()) / norm.float())
-            self.loss += self.criterion(x.unsqueeze(0), y.unsqueeze(0))
+            self.loss += self.criterion(x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1).float())
             #loss = self.criterion(x.contiguous().view(-1, x.size(-1)), y.contiguous().view(-1).float()) / norm.float()
             print(self.loss)
         except Exception as e:
